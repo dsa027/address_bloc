471ea9630225c3066d79f5a2817d269e60c090f6
David Alderson <dsa027@gmail.com>
2018-01-21 14:51:08 -0700

    Process input csv file

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative '../models/entry'
+require 'csv'
 
 class AddressBook
   attr_reader :entries
@@ -10,10 +11,10 @@ class AddressBook
   def add_entry(name, phone_number, email)
     index = 0
     entries.each do |entry|
-      if (name < entry.name)
+      if (entry.name > name)
         break
       end
-      ++index
+      index += 1
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
@@ -24,6 +25,13 @@ class AddressBook
     end
     if (!entry.nil?)
       entries.delete(entry)
+    end
+  end
+
+  def import_from_csv(file_name)
+    CSV.foreach(file_name, {headers: true, skip_blanks: true}) do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
 end
